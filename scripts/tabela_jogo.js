7266269d65fa8d74d1951650fc1c42dcca419e64
Hat-Boi <eu.tou.aqui.15@gmail.com>
2023-05-14 21:41:33 +0100

    jogo

--- scripts/tabela_jogo.js
+++ scripts/tabela_jogo.js
@@ -1,5 +1,12 @@
 PECAS=[];
 
+// DEFINIR O TAMANHO DA TABELA
+
+ALTURA_TABELA=8;
+LARGURA_TABELA=8;
+
+window.addEventListener("load", onload);
+
 const cadaPeca = [
     new peca ("azul","media/por/blueGem.png"),
     new peca ("verde","media/por/greenGem.png"),
@@ -10,29 +17,47 @@ const cadaPeca = [
     new peca ("amarelo","media/por/yellowGem.png")
 ]
 
-window.addEventListener("load", onload);
-
-function onload() {
-    desenharTabela()
+function peca (id,imagem){
+    this.id=id;
+    this.imagem=imagem;
 }
 
-function peca (id,imagem){
-    this.id=id
-    this.imagem=imagem
+function onload() {
+    console.log("hello")
+    desenharTabela();
+    console.log(desenharTabela());
 }
 
 function desenharTabela(){
     tabela=[]
-    for (let i = 0; i < 8; i++){
-        linha=desenharRow()
-        tabela.push("<tr>"+ linha +"</tr>")
+    for (let i = 0; i < ALTURA_TABELA; i++){
+        linha=desenharRow();
+        tabela.push("<tr>"+ linha +"</tr>");
     }
     document.getElementById("tabelajogo").innerHTML = tabela;
+    return tabela;
 }
 
 function desenharRow(){
-    linha=[]
-    for (let i = 0; i < 8; i++){
-        return linha.push("<td>" + PECAS.forEach((Math.floor(Math.random() * 8)) + "</td>"))
+    novaLinha=[]
+    for (let i = 0; i < LARGURA_TABELA; i++){
+        novaLinha.push(generarPeca());
+        novaLinha = verSeHaTresEmLinha(novaLinha);
     }
+    return novaLinha;
+}
+
+function generarPeca(){
+    return cadaPeca[Math.floor(Math.random()*cadaPeca.length)];
+}
+
+function verSeHaTresEmLinha(linha){
+    for (let i=0; i < linha.lenght; i++){
+        if (linha[i] == linha[i+1]){
+            linha.pop(i+1);
+            linha.push(generarPeca());
+            verSeHaTresEmLinha(linha);
+        }
+    }
+    return linha;
 }
