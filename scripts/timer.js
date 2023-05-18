6df0f4082a5dd166fe18e8e4b74b53bdb4a014e8
dipicarra <icldiogo@gmail.com>
2023-05-18 12:11:47 +0100

    timer fixed

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,9 +1,14 @@
 
 function timer(){
-    // sec é o tempo do timer
-    var sec = 0;
+    let sec = 0;
+    let min = 0
     var timer = setInterval(function(){
-        document.getElementById('timer').innerHTML='Time: 00:'+sec;
+        document.getElementById('timer').innerHTML='Time:' +min +':' +sec;
+        
+        if (sec == 60) {
+            min++;
+            sec = 0;
+        }
         sec++;
     // 1000 é o nº de milisegundos entre "ticks" ou seja a cada 1s sofre update
     }, 1000);
