b4418427863b0c6d25a0268fb4988d9a8e4fa596
dipicarra <icldiogo@gmail.com>
2023-05-18 11:30:17 +0100

    bugs / timer
    timer agora é um cronometro, bug fixes do caralho mais antigo , um start para o jogo

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,15 +1,10 @@
 
 function timer(){
     // sec é o tempo do timer
-    var sec = 30;
+    var sec = 0;
     var timer = setInterval(function(){
-        document.getElementById('timer').innerHTML='00:'+sec;
-        sec--;
-        // fim do timer
-        if (sec < 0) {
-            clearInterval(timer);
-            alert("Time's up!");
-        }
+        document.getElementById('timer').innerHTML='Time: 00:'+sec;
+        sec++;
     // 1000 é o nº de milisegundos entre "ticks" ou seja a cada 1s sofre update
     }, 1000);
 }
